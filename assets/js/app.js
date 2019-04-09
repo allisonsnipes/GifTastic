27c7b9043d14f5a339e0e89212b917e6a90965f7
Allison Snipes <ajsnipes@usfca.edu>
2019-04-08 19:42:36 -0700

    working on text field

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+let queryURL = {
+  url:
+  apiKey: ZjUY0xPzP6S5TW9QVWkwsTP2dVWs2fkf;
+  limit: 20;
+}
+
+
+//event listener for all button elements on a page
+document.querySelector(".container").addEventListener("click", function(event) {
+
+  //why do i have an .toUpperCase here?
+  if (event.target.tagName === "button".toUpperCase()) {
+    document.getElementById("textBox").val.trim();
+
+  }
+
+  //constructing a URL to search giphy
+});
