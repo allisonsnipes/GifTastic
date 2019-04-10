d6263c8fc0f2278570df6fc27c25b81d9112b4ba
Allison Snipes <ajsnipes@usfca.edu>
2019-04-09 18:15:25 -0700

    refactoring js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,18 @@ document.querySelector("#container").addEventListener("click", function(event) {
   })
   //we now use the .then method to save the response again as an itialized results variable (of the data)
   var results = response.data;
+
+  //using the new "let.. of loop" to loop over every result item in object(?)
+  for (let item of results) {
+    //institute the rating for the  images of not allowing r and pg13 raings!
+    if(item.rating !== "r" && item.rating !== "pg-13") {
+      //creating the div element to display the giphy result images
+      var giphyDiv = document.createElement("div");
+
+      //initalize the ratings variable for the giphy images
+      var rating = item.rating;
+    }
+  }
 });
 
 //document.getElementById("textBox").val.trim();
