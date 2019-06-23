d670dd28712f79da1dab85b3316e8db50efd7a50
Allison Snipes <ajsnipes@usfca.edu>
2019-06-23 10:45:38 -0700

    changing out for vanilla js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,72 @@
-//event listener for all button elements on a page
-document.querySelector("#clickMe").addEventListener("click", function(event) {
-
-  //why uppercase?
-  if (event.target.tagName == "BUTTON") {
-    console.log(event);
+// event listener for all button elements on a page
+document.querySelector('container').addEventListener('click', function (event) {
+  // why uppercase?
+  if (event.target.tagName == 'BUTTON') {
+    console.log(event)
 
     //replaces spaces with '___' with "+"
-    var buttons = event.target.dataset.buttons.replace(" ", "+");
+    var buttons = event.target.dataset.buttons.replace(' ', '+')
 
     //constructing search query URL for button name
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q="${buttons}&api_key=ZjUY0xPzP6S5TW9QVWkwsTP2dVWs2fkf&limit=20`;
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q="${buttons}&api_key=ZjUY0xPzP6S5TW9QVWkwsTP2dVWs2fkf&limit=20`
 
     //AJAX request
     fetch(queryURL, {
-      method: "GET"
+      method: 'GET'
     })
 
-    //now the data comes back from the API and we use the .then method to return a promise of the JSON returned
-    .then(function(response) {
-      return response.json();
+    // now the data comes back from the API and we use the .then method to return a promise of the JSON returned
+      .then(function (response) {
+        return response.json()
     })
 
-    //need help/explaination on this part
-    .then(function(response) {
-      //check up on results
-      console.log(queryURL);
-      console.log("im waiting", response);
+    // need help/explaination on this part
+      .then(function (response) {
+      // check up on results
+        console.log(queryURL)
+      console.log('im waiting', response)
       //we now use the .then method to save the response again as an itialized results variable (of the data)
-      var results = response.data;
+      var results = response.data
 
       //using the new "let.. of loop" to loop over every result item in object(?)
       for (let item of results) {
-        //institute the rating for the  images of not allowing r and pg13 raings!
-        if(item.rating !== "r" && item.rating !== "pg-13") {
-          //creating the div element to display the giphy result images
-          var giphyDiv = document.createElement("div");
+        // institute the rating for the  images of not allowing r and pg13 raings!
+          if (item.rating !== 'r' && item.rating !== 'pg-13') {
+          // creating the div element to display the giphy result images
+            var giphyDiv = document.createElement('div')
 
           //initalize the ratings variable for the giphy images and store them in rating variable
-          var rating = item.rating;
+          var rating = item.rating
 
           //create an element to display the rating of the image
-          var p = document.createElement("p");
+          var p = document.createElement('p')
           //this is where the actual rating shows in the element from the interpolated string
-          p.innerText = `Image Rating: ${rating}`;
+          p.innerText = `Image Rating: ${rating}`
 
           //initialize a variable that will have the img tag that we need
-          var buttonImg = document.createElement("img");
+          var buttonImg = document.createElement('img')
 
           //put on the img tag the source attribute from whats on the results JSON gathered
-          buttonImg.setAttribute("src", item.images.fixed_height.url);
+          buttonImg.setAttribute('src', item.images.fixed_height.url)
 
           //attach as a child element the p and buttonImg to the giphyDiv (first declared above)
-          giphyDiv.appendChild(p);
-          giphyDiv.appendChild(buttonImg);
+          giphyDiv.appendChild(p)
+          giphyDiv.appendChild(buttonImg)
 
           //prepend the giphyDiv to giphyImgHere div. but first create the container to do so then select where it is to appear in the DOM.
-          let giphyContainer = document.querySelector("#giphyImgHere");
+          let giphyContainer = document.querySelector('#giphyImgHere')
           //insert before new child then the reference child
-          giphyContainer.insertBefore(giphyDiv, giphyContainer.firstChild);
+          giphyContainer.insertBefore(giphyDiv, giphyContainer.firstChild)
         }
-      }
-    });
-    textGatherInput();
+        }
+      })
+    textGatherInput()
   }
 
-  //new function for grabbing text inbox
-   function textGatherInput() {
-    document.querySelector("clickMe").addEventListener("click", function(event) {
-      document.getElementById("textBox").val.trim();
+  // new function for grabbing text inbox
+  function textGatherInput () {
+    document.querySelector('clickMe').addEvent; Listener('click', function (event) {
+      document.getElementById('textBox').val.trim()
     })
-
   }
-});
-
+})
